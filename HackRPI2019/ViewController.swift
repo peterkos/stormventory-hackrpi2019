0fa8c23ae7233a11cb6c6b3a5c6d519feca41f2f
Peter Kos <pkos91@icloud.com>
2019-11-02 16:15:51 -0400

    Initial Commit

---
+++ HackRPI2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackRPI2019
+//
+//  Created by Peter Kos on 11/2/19.
+//  Copyright © 2019 RIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
