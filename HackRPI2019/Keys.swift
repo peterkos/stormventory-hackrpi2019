acbd1da3ac296585af6a2e106a79ff440398c22e
Peter Kos <pkos91@icloud.com>
2019-11-02 17:48:34 -0400

    Key placeholders, added swiftyjson

---
+++ HackRPI2019/Keys.swift
@@ -0,0 +1,16 @@
+//
+//  Keys.swift
+//  HackRPI2019
+//
+//  Created by Peter Kos on 11/2/19.
+//  Copyright © 2019 RIT. All rights reserved.
+//
+
+import Foundation
+
+
+// API things (mainly for IBM Cloud)
+
+let apikey = "API_KEY_HERE"
+let apiurl = "https://gateway.watsonplatform.net/visual-recognition/api"
+
